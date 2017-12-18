69201c96f2d96c394d7f66c6af6c05815981fca8
Caryn McCarthy <cmccarthy15@gmail.com>
2017-12-18 14:47:45 -0500

    working on parsing csv

---
+++ data/functions.js
@@ -0,0 +1,2 @@
+// 1. Write a function that takes a candidate_id and returns their percentile for their coding and commmunication score compared to other candidates at the same title and at similar companies.
+
